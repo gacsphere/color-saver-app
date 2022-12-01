f0728f672f3dd870bb884c5e3bb1a5402ca8dd95
Gesa Siebert <hallo@gesasiebert.de>
2022-12-01 11:40:23 +0100

    add array of initial colors to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,70 @@
 import "./App.css";
+import { nanoid } from "nanoid";
 
 function App() {
+  const initialColors = [
+    {
+      id: nanoid(),
+      colorCode: "#99115B",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#E6D247",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#E63093",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#19D1E6",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#188C99",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#99115B",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#E6D247",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#E63093",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#19D1E6",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#188C99",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#99115B",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#E6D247",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#E63093",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#19D1E6",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#188C99",
+    },
+  ];
+
   return <></>;
 }
 
